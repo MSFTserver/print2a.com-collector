85f128f64051af19ca2e882732facad8a8ccb74b
MSFTserver <MSFTserver@gmail.com>
2023-03-09 13:21:22 -0800

    revert to manual downloads

--- dl.py
+++ dl.py
@@ -17,10 +17,10 @@ lbrytools.ch_download_latest() for that many entries.
 from datetime import datetime
 from typing import Optional
 import lbrytools as lt
-import os, sys, argparse, shutil, patoolib, re, time
+import os, sys, argparse, shutil, patoolib, re, time, json
 
 cwd = os.getcwd()
-dl_path = "D:\\print2a-master-folder\\p2aup-test"
+dl_path = "D:\\print2a-master-folder\\test"
 unfriendly = ["?","!","[","]",";",":","*","/","\\","}","{","(",")","'",'"']
 reg_unfriendly = ["?","!","[","\]",";",":","*","/","\\","}","{","(",")","'",'"']
 video_ext_list = ['.3g2', '.3gp', '.amv', '.asf', '.avi', '.f4a', '.f4b', '.f4p',
@@ -59,27 +59,28 @@ MAX_SEARCH_CLAIMS = 10000
 SERVER = 'http://localhost:5279'
 TS_FORMAT = '%Y-%m-%d'
 
-def find_num_downloads(channel: str, dt: datetime) -> int:
-    '''
-    Search a channel for the number of claims since a specified datetime.
-    '''
-    def _filter_claims_by_date(x):
-        '''
-        Filter the claims by their date.
-        Compare with dt.
-        '''
-        return datetime.fromtimestamp(
-                int(x['meta']['creation_timestamp']), # Unix ts
-            ) > dt
-    try:
-        all_claims = lt.ch_search_n_claims(channel,
-                                           number=MAX_SEARCH_CLAIMS,
-                                           server=SERVER).get('claims')
-        return len(list(filter(_filter_claims_by_date, all_claims)))
-    except (TypeError, KeyError, ValueError):
-        # Our search fucked up. We can potentially try
-        # to handle it here, but for now just immediately raise it.
-        raise
+# def find_num_downloads(channel: str, dt: datetime) -> int:
+#     '''
+#     Search a channel for the number of claims since a specified datetime.
+#     '''
+#     def _filter_claims_by_date(x):
+#         '''
+#         Filter the claims by their date.
+#         Compare with dt.
+#         '''
+#         return datetime.fromtimestamp(
+#                 int(x['meta']['creation_timestamp']), # Unix ts
+#             ) > dt
+#     try:
+#         all_claims = lt.ch_search_n_claims(channel,
+#                                            number=MAX_SEARCH_CLAIMS,
+#                                            server=SERVER).get('claims')
+#         return len(list(filter(_filter_claims_by_date, all_claims)))
+#     except (TypeError, KeyError, ValueError):
+#         # Our search fucked up. We can potentially try
+#         # to handle it here, but for now just immediately raise it.
+#         raise
+
 
 def traverse_dir(dir):
     '''
@@ -97,6 +98,7 @@ def traverse_dir(dir):
         if len(subdir) == 1 and not len(listfilename):
             for dir,subdir,listfilename in os.walk(os.path.join(dir,subdir[0])):
                 for new_dir in subdir:
+                    print(os.path.join(dir,new_dir),dir.rsplit(os.path.sep, 1)[0])
                     shutil.move(os.path.join(dir,new_dir),dir.rsplit(os.path.sep, 1)[0])
                 for filename in listfilename:
                     shutil.move(os.path.join(dir,filename),dir.rsplit(os.path.sep, 1)[0])
@@ -106,7 +108,7 @@ def traverse_dir(dir):
                 subdir = [ name for name in os.listdir(dir) if os.path.isdir(os.path.join(dir, name)) ]
                 traverse_dir(dir)
 
-def remove_dup_folders(rm_folder,project_name):
+def remove_dup_folders(rm_folder):
     '''
     Remove duplicate folders due to extra folders created by extracting files
      From This   -->   Into this
@@ -132,13 +134,16 @@ def remove_dup_folders(rm_folder,project_name):
         
 
 
-def extract_archives(root_path, channel_name):
+def extract_archives(root_path):
+# def extract_archives(root_path, channel_name, p2a_desc, dl_desc, dl_dir, dl_filename, new_filename):
+
     '''
     Extract all archives in the given path
     also sorts out images, videos and audio files into seperate directories
     if a file doesnt match any of these it is put in its own folder with same name as the file
     '''
     for dir,subdir,listfilename in os.walk(root_path):
+            print(root_path)
             for file in os.listdir(dir):
                 name, ext = os.path.splitext(file)
                 out_path = os.path.join(root_path, name)
@@ -147,88 +152,94 @@ def extract_archives(root_path, channel_name):
                     os.makedirs(out_path, exist_ok=True)
                     print(f'Extracting {name}')
                     patoolib.extract_archive(os.path.join(root_path, dir, file), outdir=out_path, verbosity=-1)
-                    remove_dup_folders(out_path,channel_name)
+                    remove_dup_folders(out_path)
+                    # remove_dup_folders(out_path,channel_name)
                     os.remove(os.path.join(root_path, dir, file))
-                elif ext in video_ext_list:
-                    video_path = os.path.join(root_path, f'{channel_name}-videos')
-                    os.makedirs(video_path, exist_ok=True)
-                    shutil.move(os.path.join(root_path, dir, file), video_path)
-                elif ext in audio_ext_list:
-                    audio_path = os.path.join(root_path, f'{channel_name}-audio')
-                    os.makedirs(audio_path, exist_ok=True)
-                    shutil.move(os.path.join(root_path, dir, file), audio_path)
-                elif ext in image_ext_list:
-                    images_path = os.path.join(root_path, f'{channel_name}-images')
-                    os.makedirs(images_path, exist_ok=True)
-                    shutil.move(os.path.join(root_path, dir, file), images_path)
+                # elif ext in video_ext_list:
+                #     video_path = os.path.join(root_path, f'{channel_name}-videos')
+                #     os.makedirs(video_path, exist_ok=True)
+                #     shutil.move(os.path.join(root_path, dir, file), video_path)
+                # elif ext in audio_ext_list:
+                #     audio_path = os.path.join(root_path, f'{channel_name}-audio')
+                #     os.makedirs(audio_path, exist_ok=True)
+                #     shutil.move(os.path.join(root_path, dir, file), audio_path)
+                # elif ext in image_ext_list:
+                #     images_path = os.path.join(root_path, f'{channel_name}-images')
+                #     os.makedirs(images_path, exist_ok=True)
+                #     shutil.move(os.path.join(root_path, dir, file), images_path)
                 elif os.path.isdir(os.path.join(root_path, dir, file)):
                     continue
                 else:
-                    misc_path = os.path.join(root_path, f'{channel_name}-misc')
-                    os.makedirs(misc_path, exist_ok=True)
-                    shutil.move(os.path.join(root_path, dir, file), misc_path)
+                    if root_path == dir:
+                        os.makedirs(os.path.join(dir,os.path.splitext(file)[0]), exist_ok=True)
+                        shutil.move(os.path.join(dir, file), os.path.join(dir,os.path.splitext(file)[0]))
+    # if "."+ext in archive_ext_list:
+    #     desc_path = os.path.join(dl_dir, new_filename, 'lbry_description.md')
+    #     f = open(desc_path, "w+")
+    #     f.write(
+    #         dl_desc+
+    #         p2a_desc
+    #     )
+    #     f.close()
+    # else :
+    #     desc_path = os.path.join(dl_dir, dl_filename, 'lbry_description.md')
+    #     f = open(desc_path, "w+")
+    #     f.write(
+    #         dl_desc+
+    #         p2a_desc
+    #     )
+    #     f.close()
     print("done extracting files!")
     
 
-def _download(channel: str,
-              max_downloads: int,
-              download_path: Optional[str] = None) -> None:
-    '''
-    Download a channel, grabbing all claims newer than download_date,
-    that aren't in the dowload_path already.
-    '''
-    downloaded_file = lt.ch_download_latest(
-        channel=channel,
-        number=max_downloads,
-        ddir=download_path,
-        save_file=True,
-        repost=False,
-        server=SERVER
-    )
-    dl_metadata = [sub['metadata'] for sub in downloaded_file]
-    dl_title = [sub['claim_name'] for sub in downloaded_file][0]
-    dl_desc = [sub['description'] for sub in dl_metadata][0]
-    dl_source = [sub['source'] for sub in dl_metadata]
-    dl_size = int([sub['size'] for sub in dl_source][0])
-    dl_filename = [sub['file_name'] for sub in downloaded_file][0]
-    dl_path = [sub['download_path'] for sub in downloaded_file][0]
-    dl_dir = [sub['download_directory'] for sub in downloaded_file][0]
-    channel_name = dl_dir.rsplit('\\',1)[1]
-    url_channel_name, url_channel_claim = channel_name.rsplit('_',1)
-    new_filename, ext = dl_filename.rsplit('.',1)
-    print(os.path.exists(dl_path))
-    file_size = os.stat(dl_path).st_size
-    print("downloading...")
-    while not dl_size == file_size:
-        file_size = os.stat(dl_path).st_size
-        print(f'{file_size}/{dl_size}')
-        time.sleep(1)
-    print(f"Extracting Archive...")
-    extract_archives(dl_dir,channel_name)
-    if "."+ext in archive_ext_list:
-        desc_path = os.path.join(dl_dir, new_filename, f'lbry_description.md')
-        f = open(desc_path, "w+")
-        f.write(
-            dl_desc+
-            f'this file was auto created by print2a.com custom lbry collector, it is the description taken from the downloaded upload on the lbry/odysee post @ https://odysee.com/{url_channel_name}:{url_channel_claim}/{dl_title} \n\n'
-        )
-        f.close()
-    else :
-        desc_path = os.path.join(dl_dir, f'lbry_description.md')
-        f = open(desc_path, "w+")
-        f.write(dl_desc)
-        f.close()
+# def _download(channel: str,
+#               max_downloads: int,
+#               download_path: Optional[str] = None) -> None:
+#     '''
+#     Download a channel, grabbing all claims newer than download_date,
+#     that aren't in the dowload_path already.
+#     '''
+#     downloaded_file = lt.ch_download_latest(
+#         channel=channel,
+#         number=max_downloads,
+#         ddir=download_path,
+#         save_file=True,
+#         repost=False,
+#         server=SERVER
+#     )
+#     dl_metadata = [sub['metadata'] for sub in downloaded_file]
+#     print(dl_metadata)
+#     dl_title = [sub['claim_name'] for sub in downloaded_file][0]
+#     dl_desc = [sub['description'] for sub in dl_metadata][0]
+#     dl_source = [sub['source'] for sub in dl_metadata]
+#     dl_size = int([sub['size'] for sub in dl_source][0])
+#     dl_filename = [sub['file_name'] for sub in downloaded_file][0]
+#     dl_path = [sub['download_path'] for sub in downloaded_file][0]
+#     dl_dir = [sub['download_directory'] for sub in downloaded_file][0]
+#     channel_name = dl_dir.rsplit('\\',1)[1]
+#     url_channel_name, url_channel_claim = channel_name.rsplit('_',1)
+#     new_filename, ext = dl_filename.rsplit('.',1)
+#     print(os.path.exists(dl_path))
+#     file_size = os.stat(dl_path).st_size
+#     p2a_desc = f'\n\nthis file was auto created by print2a.com custom lbry collector, it is the description taken from the downloaded upload on the lbry/odysee post @ https://odysee.com/{url_channel_name}:{url_channel_claim}/{dl_title} \n\n'
+#     print("downloading...")
+#     while not dl_size == file_size:
+#         file_size = os.stat(dl_path).st_size
+#         print(f'{file_size}/{dl_size}')
+#         time.sleep(1)
+#     print(f"Extracting Archive...")
+#     extract_archives(dl_dir,channel_name,p2a_desc,dl_desc,dl_dir,dl_filename,new_filename)
 
-def download_channel(channel: str,
-                     download_date: datetime,
-                     download_path: Optional[str] = None) -> None:
-    '''
-    Download a channel. First fetch info on the number of downloads we should
-    be doing using find_num_downloads(), then run the download process
-    with _download().
-    '''
-    num_downloads = find_num_downloads(channel, download_date)
-    _download(channel, num_downloads, download_path)
+# def download_channel(channel: str,
+#                      download_date: datetime,
+#                      download_path: Optional[str] = None) -> None:
+#     '''
+#     Download a channel. First fetch info on the number of downloads we should
+#     be doing using find_num_downloads(), then run the download process
+#     with _download().
+#     '''
+#     num_downloads = find_num_downloads(channel, download_date)
+#     _download(channel, num_downloads, download_path)
 
 def rename(dir, is_dir, filename, new_filename, join_filenames):
     '''
@@ -300,6 +311,7 @@ def make_friendly(path,self_called=False):
     if it is a file, it will be renamed to a friendly name
     if the directory includes subdirs we recursively call this function on them.
     '''
+    remove_dup_folders(path)
     for dir,subdir,listfilename in os.walk(path):
         if not self_called:
             print(dir.replace(dl_path,""))
@@ -315,37 +327,38 @@ def main() -> None:
     '''
     Argument parser and main function.
     '''
-    parser = argparse.ArgumentParser(description='Run a fetch of a LBRY channel')
-    parser.add_argument('-p', '--path', dest='path',
-                        type=str, required=False)
-    parser.add_argument('-d', '--after-date', dest='after_date',
-                        type=str, required=False)
-    args = parser.parse_args()
-    if not args.path:
-        download_path = dl_path
-    else:
-        download_path = args.path
-    if not args.after_date:
-        dt = datetime.fromtimestamp(0)  # First unix timestamp
-    else:
-        try:
-            dt = datetime.strptime(args.after_date, TS_FORMAT)
-        except (TypeError, ValueError) as err:
-            print(f'INVALID date: {args.after_date}')
-            print(f'Use timestamp format: {TS_FORMAT}')
-            print(f'Exception caught: {repr(err)}')
-            sys.exit(1)
-    print(f'Downloading from {dt} to current time')
-    print(f'Downloading to {download_path}')
-    print(f'Downloading from a total of {len(channels_list)} channels')
-    for channel_name in channels_list:
-        try:
-            print(f'Downloading from {channel_name}...')
-            download_channel(channel_name, dt, download_path)
-        except (TypeError, ValueError, KeyError) as err:
-            print(f'Caught exception while downloading channel {channel_name}')
-            print(f'Exception caught: {repr(err)}')
-            sys.exit(1)
+    # parser = argparse.ArgumentParser(description='Run a fetch of a LBRY channel')
+    # parser.add_argument('-p', '--path', dest='path',
+    #                     type=str, required=False)
+    # parser.add_argument('-d', '--after-date', dest='after_date',
+    #                     type=str, required=False)
+    # args = parser.parse_args()
+    # if not args.path:
+    #     download_path = dl_path
+    # else:
+    #     download_path = args.path
+    # if not args.after_date:
+    #     dt = datetime.fromtimestamp(0)  # First unix timestamp
+    # else:
+    #     try:
+    #         dt = datetime.strptime(args.after_date, TS_FORMAT)
+    #     except (TypeError, ValueError) as err:
+    #         print(f'INVALID date: {args.after_date}')
+    #         print(f'Use timestamp format: {TS_FORMAT}')
+    #         print(f'Exception caught: {repr(err)}')
+    #         sys.exit(1)
+    # print(f'Downloading from {dt} to current time')
+    # print(f'Downloading to {download_path}')
+    # print(f'Downloading from a total of {len(channels_list)} channels')
+    # for channel_name in channels_list:
+    #     try:
+    #         print(f'Downloading from {channel_name}...')
+    #         download_channel(channel_name, dt, download_path)
+    #     except (TypeError, ValueError, KeyError) as err:
+    #         print(f'Caught exception while downloading channel {channel_name}')
+    #         print(f'Exception caught: {repr(err)}')
+    #         sys.exit(1)
+    extract_archives(dl_path)
     print(f"Sanitizing folder and file names...")
     print("Checked Directories : (list will populate as directories are traversed)")
     make_friendly(dl_path)
